8373e27abc42f1a20e40dcf6b1db170f992e66aa
Junhyeok Jeong <jeongju@flip2.engr.oregonstate.edu>
2020-05-19 03:10:44 -0700

    pre-processing is done

--- assignments/assignment_3/restaurant_classifier.py
+++ assignments/assignment_3/restaurant_classifier.py
@@ -35,12 +35,7 @@ def clean_text(text):
 
 def create_bow(input, vocabulary):
     vectorizer = CountVectorizer(
-        analyzer = "word",
-        tokenizer=None,
-        preprocessor=clean_text,
-        stop_words=None,
-        vocabulary=vocabulary,
-        max_features=2000
+        analyzer = 'word'
     )
     bag_of_words = vectorizer.fit_transform(input)
     bag_of_words = bag_of_words.toarray()
@@ -50,39 +45,126 @@ def create_bow(input, vocabulary):
     return bag_of_words, name
 
 if __name__ == "__main__":
+    # training set pre-processing
+
     # Importing the dataset
     with open("trainingSet.txt", "r") as f:
-        sentences = []
-        labels = []
+        train_sentences = []
+        train_labels = []
     #read and copy every line on an array called 'lines'
         for i, line in enumerate(f):
-            split = line.strip().split('\t')
-            sentences.append(clean_text(split[0]))
-            labels.append(int(split[1]))
+            splitting = line.strip().split('\t')
+            train_sentences.append(clean_text(splitting[0]))
+            train_labels.append(int(splitting[1]))
 
-    print(sentences)
-    print(labels)
+    print(train_sentences[41])
+    #print(train_labels)
     
+    # Pre-processing step
     vectorizer = CountVectorizer(
-    stop_words="english"
+    analyzer = "word"
     )
-
+    X = vectorizer.fit_transform(train_sentences)
+    #print(len(vectorizer.get_feature_names()), vectorizer.get_feature_names())
     # fit the vectorizer on the text
-    vectorizer.fit(sentences)
+    vectorizer.fit(train_sentences)
     # get the vocabulary
     inv_vocab = {v: k for k, v in vectorizer.vocabulary_.items()}
-    vocabulary = [inv_vocab[i] for i in range(len(inv_vocab))]
-    print(vocabulary)
+    train_vocabulary = [inv_vocab[i] for i in range(len(inv_vocab))]
+    print(len(train_vocabulary))
 
-    # 1. generate BOW for all restaurant reviews
-    print("generating BOW for restaurant reviews ...")
-    bow, name = create_bow(sentences, vocabulary)
-    bow = np.sum(bow, axis=0)
+    # generate BOW for all restaurant reviews
+    print("generating train BOW of restaurant reviews ...")
+    bow, name = create_bow(train_sentences, train_vocabulary)
 
-    model = pd.DataFrame( 
-    (count, word) for word, count in
-    zip(bow, name))
-    model.columns = ['Word', 'Count']
-    print(model)
-    print("done ... !")
+    np_train_labels = np.array(train_labels)[np.newaxis].T
     
+    bow = np.append(bow, np_train_labels, 1)
+    for i in range(len(bow)):
+        for j in range(len(bow[i]) - 1):
+            if bow[i][j] > 0:
+                bow[i][j] = 1
+
+    # delete later
+    print(bow[120])
+    print(len(bow[120]))
+    print(len(bow))
+    counter = 0
+    for i in range(len(bow[120]) - 1):
+        if bow[120][i] == 1:
+            counter += 1
+    #
+
+    # write pre-processed files
+    np.savetxt('preprocessed_train.txt', bow, fmt="%d", delimiter=',')
+
+    with open('preprocessed_train.txt', 'r+') as res:
+        content = res.read()
+        res.seek(0, 0)
+        res.write(','.join(train_vocabulary) + ",class_label\n" + content)
+
+    print(len(train_labels))
+    print(counter)
+    print("done ... !")
+
+    # test set pre-processing
+
+    # Importing the dataset
+    with open("testSet.txt", "r") as t_f:
+        test_sentences = []
+        test_labels = []
+    #read and copy every line on an array called 'lines'
+        for i, line in enumerate(t_f):
+            splitting = line.strip().split('\t')
+            test_sentences.append(clean_text(splitting[0]))
+            test_labels.append(int(splitting[1]))
+
+    print(test_sentences[41])
+    #print(train_labels)
+    
+    # Pre-processing step
+    vectorizer = CountVectorizer(
+    analyzer = "word"
+    )
+    X = vectorizer.fit_transform(test_sentences)
+    #print(len(vectorizer.get_feature_names()), vectorizer.get_feature_names())
+    # fit the vectorizer on the text
+    vectorizer.fit(test_sentences)
+    # get the vocabulary
+    inv_vocab = {v: k for k, v in vectorizer.vocabulary_.items()}
+    test_vocabulary = [inv_vocab[i] for i in range(len(inv_vocab))]
+    print(len(test_vocabulary))
+
+    # generate BOW for all restaurant reviews
+    print("generating train BOW of restaurant reviews ...")
+    test_bow, name = create_bow(test_sentences, test_vocabulary)
+
+    np_test_labels = np.array(test_labels)[np.newaxis].T
+    
+    test_bow = np.append(test_bow, np_test_labels, 1)
+    for i in range(len(test_bow)):
+        for j in range(len(test_bow[i]) - 1):
+            if test_bow[i][j] > 0:
+                test_bow[i][j] = 1
+
+    # delete later
+    print(test_bow[120])
+    print(len(test_bow[120]))
+    print(len(test_bow))
+    counter = 0
+    for i in range(len(test_bow[120]) - 1):
+        if test_bow[120][i] == 1:
+            counter += 1
+    #
+
+    # write pre-processed files
+    np.savetxt('preprocessed_test.txt', test_bow, fmt="%d", delimiter=',')
+
+    with open('preprocessed_test.txt', 'r+') as t_res:
+        content = t_res.read()
+        t_res.seek(0, 0)
+        t_res.write(','.join(test_vocabulary) + ",class_label\n" + content)
+
+    print(len(test_labels))
+    print(counter)
+    print("done ... !")
